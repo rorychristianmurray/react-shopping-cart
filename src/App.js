31264cee62291789fcf679b14936ab2c4920d527
Rory Murray <rory.christian.murray@gmail.com>
2019-07-22 17:12:31 -0400

    Refactor ShoppingCart using context

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
           {/* Routes */}
           <Route exact path="/" component={Products} />
 
-          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" component={ShoppingCart} />
         </CartContext.Provider>
       </ProductContext.Provider>
     </div>
